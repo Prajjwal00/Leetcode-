3d1f59df5a4d1cabc6e4c2ba967d9d390d4ef89b
Prajjwal Sharma <68287383+Prajjwal00@users.noreply.github.com>
2022-01-21 12:00:58 +0530

    Create subs.py

---
+++ subs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        dict1={}
+        start=0
+        max_len=0
+        for i in range(0,len(s)):
+            if s[i] in dict1:
+                start=max(start,dict1[s[i]]+1)
+            max_len=max(max_len,i-start+1)
+            dict1[s[i]]=i
+        return max_len
