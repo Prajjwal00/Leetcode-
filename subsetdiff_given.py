4710a2b41267a2eb2da5448cd3793a515eb8d05a
Prajjwal Sharma <68287383+Prajjwal00@users.noreply.github.com>
2022-01-25 17:44:39 +0530

    Create subsetdiff_given.py

---
+++ subsetdiff_given.py
@@ -0,0 +1,7 @@
+#targetsum problem == subsetdiff_given
+def subsetdiff_given(arr,diff):
+  if 2*(diff+len(arr)/2%2!=0:
+        return 0
+  else:
+    return subsetdiff(2*(diff+len(arr)/2,arr):
+            
